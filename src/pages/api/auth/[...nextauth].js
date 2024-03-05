67aaf775d33e94b893d83d0365fcd55ff63ab33d
Rob <barrrobert84@gmail.com>
2024-03-05 21:23:46 +0100

    Adding new env variables

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -21,7 +21,7 @@ export const authOptions = {
                 };
             },
         }),
-        // ...add more providers here
+        
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
